5a4bf7625eac5b880b4d6b832f8255eee2b4d293
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2025-02-19 00:42:09 -0800

    Inventory overhaul

--- Player/Player.gd
+++ Player/Player.gd
@@ -27,8 +27,8 @@ func _physics_process(delta: float) -> void:
 			SPEED = 5.0
 			JUMP_VELOCITY = 3
 	handle_movement(delta)
+	handle_inventory()
 	$CanvasLayer/FPS.text = str(Engine.get_frames_per_second())
-
 
 func handle_movement(delta) -> void:
 	if not is_on_floor():
@@ -47,4 +47,65 @@ func handle_movement(delta) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	move_and_slide()
-	
+
+#region Inventory
+var inventory = [null,null,null,null]
+var inventory_index:int = 0
+@export var item_cast:RayCast3D
+@export var held_item_display:Sprite3D
+var hovered_item:LevelItem
+signal item_hovered
+signal item_unhovered
+signal inventory_updated
+signal active_inventory_slot_changed(to:int)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("LeftClick"):
+		if hovered_item:
+			var item = hovered_item.item
+			hovered_item.picked_up(self)
+			hovered_item = null
+			hold_item(item)
+		return
+
+	if event.is_action_pressed("F"):
+		drop_item()
+		return
+
+	if event.is_action_pressed("1"):
+		inventory_index = 0
+	if event.is_action_pressed("2"):
+		inventory_index = 1
+	if event.is_action_pressed("3"):
+		inventory_index = 2
+	if event.is_action_pressed("4"):
+		inventory_index = 3
+	emit_signal("active_inventory_slot_changed",inventory_index)
+
+
+func handle_inventory():
+	var collider = item_cast.get_collider()
+	if collider == null:
+		if hovered_item:
+			hovered_item = null
+			emit_signal("item_unhovered")
+		return
+	hovered_item = collider
+	emit_signal("item_hovered")
+
+func hold_item(item:Item) -> void:
+	drop_item()
+	inventory[inventory_index] = item
+	held_item_display.texture = load(item.image_path())
+	emit_signal("inventory_updated")
+
+func drop_item(index:int = -1):
+	if index == -1:
+		index = inventory_index
+	if inventory[index] == null:
+		return
+	Global.level.add_item(inventory[index], global_position, self)
+	inventory[index] = null
+	held_item_display.texture = null
+	emit_signal("inventory_updated")
+#endregion
--- Player/UI/PlayerUI.gd
+++ Player/UI/PlayerUI.gd
@@ -2,12 +2,11 @@ extends Control
 
 var player:Player
 @export var health:Control
-@export var inventory:Control
+@export var item_hover:VBoxContainer
 
 func _ready() -> void:
 	player = get_parent().get_parent()
 	$MarginContainer/VBoxContainer/PlayerHealth.init(player)
-	$MarginContainer/VBoxContainer/PlayerHealth.set_size(inventory.size)
 
 func fade_in():
 	return
@@ -19,3 +18,34 @@ func fade_out(time:float = 10.0):
 	var final_color = Color(0,0,0,1.0)
 	var tween = create_tween()
 	tween.tween_property($ColorRect, "modulate", final_color, time)
+
+func _on_item_hovered() -> void:
+	item_hover.get_node("HandIcon").visible = true
+	
+	var item = player.hovered_item.item
+	var text:String = "LeftClick: Pickup " + item.item_name()
+	if item.is_consumable():
+		text += "\nRightClick: Consume"
+	item_hover.get_node("ItemPrompts").text = text
+
+func _on_item_unhovered() -> void:
+	item_hover.get_node("HandIcon").visible = false
+	item_hover.get_node("ItemPrompts").text = ""
+
+func _on_inventory_updated() -> void:
+	var ui_slots = $MarginContainer/VBoxContainer/SlotContainer.get_children()
+	for index in player.inventory.size():
+		ui_slots[index].init(player.inventory[index])
+
+var previously_active:int
+func _on_player_active_inventory_slot_changed(to:int) -> void:
+	var ui_slots = $MarginContainer/VBoxContainer/SlotContainer.get_children()
+	ui_slots[previously_active].deactivate()
+	ui_slots[to].activate()
+	previously_active = to
+	
+	var label = $MarginContainer/VBoxContainer/ActiveItem
+	if player.inventory[to]:
+		label.text = player.inventory[to].item_name()
+	else:
+		label.text = ""
