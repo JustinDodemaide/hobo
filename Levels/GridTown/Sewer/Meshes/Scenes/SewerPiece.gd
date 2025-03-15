5e2606ba48bf32460b8b63a219e46a82424ce35b
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2025-03-14 20:30:35 -0700

    3D sewer

--- Levels/GridTown/Sewer/Meshes/Scenes/SewerPiece.gd
+++ Levels/GridTown/Sewer/Meshes/Scenes/SewerPiece.gd
@@ -1,5 +1,37 @@
 extends Node3D
 class_name SewerPiece
 
-func make_horiztonal():
-	rotation_degrees = Vector3(0, 90, 0)
+@export var spawn:Marker3D
+
+	#"vertical": Vector2i(1, 0),
+	#"horizontal": Vector2i(2, 0),
+	#"corner_ne": Vector2i(3, 0),
+	#"corner_es": Vector2i(4, 0),
+	#"corner_sw": Vector2i(5, 0),
+	#"corner_wn": Vector2i(6, 0),
+	#"t_junction_n": Vector2i(7, 0),
+	#"t_junction_e": Vector2i(8, 0),
+	#"t_junction_s": Vector2i(9, 0),
+	#"t_junction_w": Vector2i(10, 0),
+	#"cross": Vector2i(11, 0),
+	#"dead_end_n": Vector2i(12, 0),
+	#"dead_end_e": Vector2i(13, 0),
+	#"dead_end_s": Vector2i(14, 0),
+	#"dead_end_w": Vector2i(15, 0)
+
+func orient(s:String):
+	match s:
+		"horizontal":rotation_degrees = Vector3(0,90,0)
+		
+		"t_junction_e":rotation_degrees = Vector3(0,-90,0)
+		"t_junction_s":rotation_degrees = Vector3(0,180,0)
+		"t_junction_w":rotation_degrees = Vector3(0,90,0)
+		
+		"corner_ne":rotation_degrees = Vector3(0,-90 + 180,0)
+		"corner_es":rotation_degrees = Vector3(0,180 + 180,0)
+		"corner_sw":rotation_degrees = Vector3(0,90 + 180,0)
+		
+		#"dead_end_n":rotation_degrees = Vector3()
+		"dead_end_e":rotation_degrees = Vector3(0,-90,0)
+		"dead_end_w":rotation_degrees = Vector3(0,90,0)
+		"dead_end_s":rotation_degrees = Vector3(0,-180,0)
--- Levels/GridTown/Sewer/Sewer.gd
+++ Levels/GridTown/Sewer/Sewer.gd
@@ -20,10 +20,34 @@ extends TileMapLayer
 	"dead_end_s": Vector2i(14, 0),
 	"dead_end_w": Vector2i(15, 0)
 }
+var meshes = {
+	"vertical": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Vertical.tscn"),
+	"horizontal": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Vertical.tscn"),
+	"corner_ne": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Corner.tscn"),
+	"corner_es": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Corner.tscn"),
+	"corner_sw": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Corner.tscn"),
+	"corner_wn": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Corner.tscn"),
+	"t_junction_n": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Junction.tscn"),
+	"t_junction_e": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Junction.tscn"),
+	"t_junction_s": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Junction.tscn"),
+	"t_junction_w": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Junction.tscn"),
+	"cross": load("res://Levels/GridTown/Sewer/Meshes/Scenes/Cross.tscn"),
+	"dead_end_n": load("res://Levels/GridTown/Sewer/Meshes/Scenes/DeadEnd.tscn"),
+	"dead_end_e": load("res://Levels/GridTown/Sewer/Meshes/Scenes/DeadEnd.tscn"),
+	"dead_end_s": load("res://Levels/GridTown/Sewer/Meshes/Scenes/DeadEnd.tscn"),
+	"dead_end_w": load("res://Levels/GridTown/Sewer/Meshes/Scenes/DeadEnd.tscn")
+}
+
+var player
 
 func _ready() -> void:
 	generate_maze()
 	identify_tunnel_types()
+	place_3d_meshes()  # New function call
+	
+	player = load("res://Player/Player.tscn").instantiate()
+	get_node("MazeGeometry").add_child(player)
+	player.global_position = get_node("MazeGeometry").get_children().pick_random().spawn.global_position
 
 func generate_maze() -> void:
 	for x in maze_width:
@@ -113,3 +137,60 @@ func determine_tile_type(conn: Dictionary) -> Vector2i:
 			return floor_tiles["cross"]
 	
 	return wall_tile
+
+# New function to place 3D meshes
+func place_3d_meshes() -> void:
+	# Create a parent node for organization
+	var mesh_parent = Node3D.new()
+	mesh_parent.name = "MazeGeometry"
+	add_child(mesh_parent)
+	
+	# Create a reverse mapping from tile coords to type strings
+	var coords_to_type = {}
+	for type_name in floor_tiles:
+		coords_to_type[floor_tiles[type_name]] = type_name
+	
+	# Place 3D meshes for each floor tile
+	for x in maze_width:
+		for y in maze_height:
+			var pos := Vector2i(x, y)
+			var atlas_coords = get_cell_atlas_coords(pos)
+			
+			# Skip walls
+			if atlas_coords == wall_tile:
+				continue
+				
+			# Get the tile type string
+			if not coords_to_type.has(atlas_coords):
+				continue
+				
+			var tile_type = coords_to_type[atlas_coords]
+			
+			# Skip if we don't have a mesh for this type
+			if not meshes.has(tile_type):
+				continue
+			
+			# Instantiate the mesh
+			var mesh_scene = meshes[tile_type]
+			var mesh_instance = mesh_scene.instantiate()
+			
+			# Position the mesh in 3D space
+			# Using x,z for horizontal position (y is up in 3D)
+			mesh_instance.position = Vector3(x, 0, y)  * 12
+			
+			# Call the orient function on the mesh, passing the tile type
+			mesh_instance.orient(tile_type)
+			
+			# Add the mesh to our parent node
+			mesh_parent.add_child(mesh_instance)
+			
+			var water = load("res://Shaders/WaterPlane.tscn").instantiate()
+			#water.scale = Vector3(100,1,100)
+			mesh_parent.add_child(water)
+
+func _process(delta: float) -> void:
+	if player == null:
+		return
+	#$Sprite2D.position = local_to_map(Vector2(player.position.x, player.position.z))
+	$Sprite2D.position.x = player.position.x / 12
+	$Sprite2D.position.y = player.position.z / 12
